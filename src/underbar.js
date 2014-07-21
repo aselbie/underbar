d02639b83b88362f2cd4ae43e2eb70a51af12808
Andrew Selbie <aselbie@gmail.com>
2014-07-21 10:49:09 -0700

    some

--- src/underbar.js
+++ src/underbar.js
@@ -191,7 +191,10 @@ var _ = {};
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
-    // TIP: There's a very clever way to re-use every() here.
+    iterator = iterator || _.identity;
+    return !_.every(collection, function(value){
+      return !iterator(value);
+    });
   };
 
 
